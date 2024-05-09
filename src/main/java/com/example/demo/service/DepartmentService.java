ff47106422e87cf78d27af3748e7abe3fba72b1b
nikhil <chaurasiyanikhil744@gmail.com>
2024-05-09 22:01:05 +0530

    send to main repository

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -5,6 +5,8 @@ import com.example.demo.entity.Department;
 import java.util.List;
 
 
+
+
 public interface DepartmentService {
     List<Department> getAllDepartments();
     Department saveDepartment(Department department);

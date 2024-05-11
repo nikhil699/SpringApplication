121c58b258a98f25fc97fbbdcd4c3b082762970d
nikhil <chaurasiyanikhil744@gmail.com>
2024-05-11 22:31:39 +0530

    ProjectServiceImpl TaskServiceImpl

--- src/main/java/com/example/demo/serviceimpl/TaskServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/TaskServiceImpl.java
@@ -14,6 +14,7 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<Task> getAllTasks() {
+
         System.out.println("fMAP-999");
         return taskRepository.findAll();
 
@@ -21,6 +22,7 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Task saveTask(Task task) {
+
         System.out.println("fMAP-999");
         return taskRepository.save(task);
     }
